641965e2327660ac2149f4d7d11b19696aac056a
franmu94 <fmalourarte@gmail.com>
2024-04-10 17:17:28 +0200

    Initial Commit

---
+++ AnimeDB/AnimeDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeDBApp.swift
+//  AnimeDB
+//
+//  Created by Fran Malo on 10/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimeDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
