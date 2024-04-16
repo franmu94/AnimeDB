5fffe999e4f52eda27f3ea8375037407a9f5b856
franmu94 <fmalourarte@gmail.com>
2024-04-16 14:24:06 +0200

    Estrellas

---
+++ AnimeDB/StarsComponent.swift
@@ -0,0 +1,18 @@
+//
+//  StarsComponent.swift
+//  AnimeDB
+//
+//  Created by Fran Malo on 16/4/24.
+//
+
+import SwiftUI
+
+struct StarsComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarsComponent()
+}
